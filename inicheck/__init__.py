a5374b633c06416f6fa0012e49aa1a10e36f205f
Micah Johnson <micah.johnson150@gmail.com>
2018-05-09 14:47:30 -0600

    Removed trigger keyword condition. Turns out science software has a need for items like initial conditions, ha!

--- inicheck/__init__.py
+++ inicheck/__init__.py
@@ -5,7 +5,7 @@ __email__ = 'micah.johnson150@gmail.com'
 __version__ = '0.2.6'
 
 # Keywords for conditional statements in a master config file
-__trigger_keywords__ = ['trigger', 'triggers', 'condition']
+__trigger_keywords__ = ['trigger']
 
 # Key words expected in the section name of a recipe
 __recipe_keywords__ = ['recipe']
