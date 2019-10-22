fefdb708349eaa54e1ae3e64bf1fb3486cb7633d
Micah Johnson <micah.johnson150@gmail.com>
2019-10-22 15:10:21 -0600

    Bump version: 0.6.0 → 0.7.0

--- inicheck/__init__.py
+++ inicheck/__init__.py
@@ -2,7 +2,7 @@
 import os
 __author__ = """Micah Johnson"""
 __email__ = 'micah.johnson150@gmail.com'
-__version__ = '0.6.0'
+__version__ = '0.7.0'
 
 # Keywords for conditional statements in a master config file
 __trigger_keywords__ = ['trigger']
