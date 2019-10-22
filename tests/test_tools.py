ef746ab6573e0fc0b83e80a67f58365130bf4222
Micah Johnson <micah.johnson150@gmail.com>
2019-10-22 14:45:53 -0600

    Fixed tools testing

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -12,6 +12,12 @@ from inicheck.tools import *
 from collections import OrderedDict
 
 class TestTools(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        base = os.path.dirname(__file__)
+        self.ucfg = get_user_config(os.path.join(base,"test_configs/full_config.ini"),
+                                                            modules="inicheck")
+
     def test_get_checkers(self):
         """
         Tests the get_checkers func in tools
@@ -20,12 +26,12 @@ class TestTools(unittest.TestCase):
 
         valids = ['bool', 'criticaldirectory', 'criticalfilename', 'datetime',
                   'datetimeorderedpair', 'directory', 'filename', 'float',
-                  'int', 'path', 'string', 'url']
+                  'int', 'string', 'url']
 
         for v in valids:
             assert v in checkers
 
-        valids = ["type", "generic"]
+        valids = ["type", "generic", "path"]
         checkers = get_checkers(ignore=[]).keys()
         for v in valids:
                 assert v in checkers
@@ -34,47 +40,34 @@ class TestTools(unittest.TestCase):
         """
         Tests the check_config func in tools
         """
-        ucfg = get_user_config("./tests/test_configs/base_cfg.ini",
-                                                            modules="inicheck")
+        ucfg = self.ucfg
 
         warnings, errors = check_config(ucfg)
-        assert not warnings
-        assert not errors
+
+        assert len(errors) == 12
 
     def test_cast_all_variables(self):
         """
         Tests the cast_all_variables func in tools
         """
-        ucfg = get_user_config("./tests/test_configs/base_cfg.ini",
-                                                            modules="inicheck")
+        ucfg = self.ucfg
+
         ucfg.cfg['topo']['test_start'] = "10-1-2019"
-        ucfg.cfg['air_temp']['dk_nthreads'] = "1.0"
+        ucfg.cfg['air_temp']['dk_ncores'] = "1.0"
         ucfg.cfg['air_temp']['detrend'] = "true"
 
-        ucfg = cast_all_variables(ucfg, ucfg.mcfg, checking_later=False)
-        results = ['timestamp','int','bool']
-        valids = [ucfg.cfg['topo']['test_start'],
-                  ucfg.cfg['air_temp']['dk_nthreads'],
-                  ucfg.cfg['air_temp']['detrend']]
+        ucfg = cast_all_variables(ucfg, ucfg.mcfg)
+        results = ['timestamp','int','bool','float','list','str']
 
-        for i,v in enumerate(valids):
+        tests = [ucfg.cfg['time']['start_date'],
+                  ucfg.cfg['air_temp']['dk_ncores'],
+                  ucfg.cfg['air_temp']['detrend'],
+                  ucfg.cfg['wind']['reduction_factor'],
+                  ucfg.cfg['output']['variables'],
+                  ucfg.cfg['air_temp']['distribution']]
+
+        for i,v in enumerate(tests):
             assert results[i] in type(v).__name__.lower()
-
-
-    def test_get_user_config(self):
-        """
-        Tests the get_user_config func in tools
-        """
-
-        # Check that we raise an error when they don't provide everything
-        with self.assertRaises(IOError):
-            ucfg = get_user_config("./tests/test_configs/base_cfg.ini")
-
-        # Check that we can handle multiple files
-        ucfg = get_user_config("./tests/test_configs/base_cfg.ini",
-                            master_files=['./tests/test_configs/CoreConfig.ini',
-                                          './tests/test_configs/recipes.ini'])
-        assert ucfg.cfg['air_temp']['slope'] == 1
 
 
 if __name__ == '__main__':
