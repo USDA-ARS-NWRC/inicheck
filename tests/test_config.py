776784d7f5f9e704cc69c0cc7b0da88ff54a6c76
Micah Johnson <micah.johnson150@gmail.com>
2019-10-28 08:36:19 -0600

    Added some recipe testing based on some recipes in smrf

--- tests/test_config.py
+++ tests/test_config.py
@@ -110,6 +110,122 @@ class TestUserConfig(unittest.TestCase):
         for v in valid_recipes:
             assert v in [r.name for r in self.ucfg.recipes]
 
+class TestRecipes(unittest.TestCase):
+
+    def setUp(self):
+        self.fname = os.path.abspath(os.path.dirname(__file__) + \
+                                    '/test_configs/full_config.ini')
+
+        self.mcfg = MasterConfig(modules='inicheck')
+        self.ucfg = UserConfig(self.fname, mcfg=self.mcfg)
+
+    def modify_cfg(self, mod_cfg):
+        """
+
+        """
+
+        for s,v in mod_cfg.items():
+            self.ucfg.raw_cfg[s] = v
+
+        self.ucfg.apply_recipes()
+
+    def check_items(self, section=None, ignore=[]):
+        """
+        Checks a section for all the items in the master config.
+
+        Args:
+            section: Section to exmaine
+            ignore: list of items to ignore useful for case when not everything
+                    is added for certain values
+        """
+        checkable = [v for v in self.mcfg.cfg[section].keys() if v not in ignore]
+
+        for v in checkable:
+            assert v in self.ucfg.cfg[section].keys()
+
+    def check_defaults(self, section=None, ignore=[]):
+        """
+        Checks a section for all the items in the master config.
+        """
+        checkable = [v for v in self.mcfg.cfg[section].keys() if v not in ignore]
+
+        for i in checkable:
+            assert self.ucfg.cfg[section][i] == self.mcfg.cfg[section][i].default
+
+
+    def test_apply_defaults(self):
+        """
+        Tests the functionality of a recipes ability to add in defaults for
+        section when a section is not there.
+        """
+
+        del self.ucfg.raw_cfg['csv']
+
+        test = {'csv':{'stations':None}}
+        self.modify_cfg(test)
+
+        self.check_items(section='csv')
+        self.check_defaults(section='csv')
+
+    def test_remove_section(self):
+        """
+        Tests the functionality of a recipes ability to a section in defaults for
+        section when a section is not there.
+        """
+
+        del self.ucfg.raw_cfg['csv']
+
+        # Order matters, since we have conflicting recipes the first one will be
+        # applied, in this case CSV will beat out gridded
+        test = {'csv':{'stations':None},
+                'gridded':{}}
+
+        self.modify_cfg(test)
+
+        self.check_items(section='csv')
+        assert 'gridded' not in self.ucfg.cfg.keys()
+
+    def test_remove_item_for_a_section(self):
+        """
+        Sometimes a recipe will remove an item when a certain section is present
+        This tests that scenario occurs, uses thermal_distribution_recipe
+        """
+
+        test = {'gridded':{'data_type':'wrf'}}
+        # The order of recipes matters. Del the csv section to avoid recipes on it
+        del self.ucfg.raw_cfg['csv']
+        self.modify_cfg(test)
+
+        assert 'distribution' not in self.ucfg.cfg['thermal'].keys()
+
+    def test_add_items_for_has_value(self):
+        """
+        Recipes have the available keyword has_value to trigger on event where
+        a section item has a value. This tests that we can trigger on it and
+        make edits. Test uses the idw_recipe in which if any section is found
+        with the item distribution set to idw, then we add idw_power and remove
+        dk_ncores.
+        """
+
+        test = {'precip':{'distribution':'idw','dk_ncores':'2'}}
+        self.modify_cfg(test)
+
+        assert 'dk_ncores' not in self.ucfg.cfg['precip'].keys()
+        assert 'idw_power' in self.ucfg.cfg['precip'].keys()
+
+    def test_apply_defaults_for_has_value(self):
+        """
+        This recipe applies defaults to items when an item has a certain value
+        This test uses the krig_recipe in which any item distribution is set to
+        kriging applies several defautls.
+        """
+
+        test = {'precip':{'distribution':'kriging','dk_ncores':'2'}}
+        self.modify_cfg(test)
+        print(self.ucfg.cfg['precip'])
+        assert 'krig_variogram_model' in self.ucfg.cfg['precip'].keys()
+        assert 'dk_ncores' not in self.ucfg.cfg['precip'].keys()
+
 
 class TestMasterConfig(unittest.TestCase):
     def setUp(self):
