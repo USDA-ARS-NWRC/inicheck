c6087857c1c45d6f49afc385ea885cbb1581208c
Micah Johnson <micah.johnson150@gmail.com>
2020-01-09 14:29:58 -0700

    Bump version: 0.7.5 → 0.7.6

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='inicheck',
-    version='0.7.5',
+    version='0.7.6',
     description="inicheck is an high level configuration file checker enabling developers tight control over their users configuration files",
     long_description=readme + '\n\n' + history,
     author="Micah Johnson",
