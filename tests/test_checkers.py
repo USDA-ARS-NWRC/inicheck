e69d59669b6c1f0ef99a1121cfe4ef4469ef81c9
Joachim Meyer <j.meyer@utah.edu>
2021-01-29 16:10:55 -0700

    PyTest - Make use of fixture scopes.
    Put all test cases into a class to utilize fixture scopes and cache the
    MasterConfig to be only loaded once for the class run.

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -55,282 +55,278 @@ def run_a_checker(
                 assert not valid
 
 
-@pytest.fixture
-def master_config(test_config_dir):
-    """
-    Master config from test data
-    """
-    return MasterConfig(
-        path=test_config_dir.joinpath('master.ini').as_posix()
-    )
-
-
-@pytest.fixture
-def user_config(test_config_dir, master_config):
-    return UserConfig(
-        test_config_dir.joinpath('base_cfg.ini').as_posix(), mcfg=master_config
-    )
-
-
-def test_string(user_config):
-    """
-    Test we see strings as strings
-    """
-    valid_entries = ['test']
-
-    run_a_checker(
-        user_config, valid_entries, [], checkers.CheckString, item='username'
-    )
-
-    # Confirm that casting a string with uppers will auto produce lowers
-    user_config.cfg['basic']['username'] = 'Test'
-
-    b = checkers.CheckString(
-        config=user_config, section='basic', item='username'
-    )
-    result = b.cast()
-    assert result == 'test'
-
-    # Check we can capture a single item list for strings
-    b.is_list = True
-    result = b.cast()
-    assert result == ['test']
-
-    # Check we capture the when a list is passed and were not expecting one
-    b.is_list = False
-    result = b.cast()
-    assert not isinstance(result, list)
-
-
-def test_bool(user_config):
-    """
-    Test we see booleans as booleans
-    """
-    valid_entries = [True, False, 'true', 'FALSE', 'yes', 'y', 'no', 'n']
-    invalid_entries = ['Fasle', 'treu']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckBool,
-        item='debug'
-    )
-
-
-def test_float(user_config):
-    """
-    Test we see floats as floats
-    """
-    valid_entries = [-1.5, '2.5']
-    invalid_entries = ['tough']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckFloat,
-        item='time_out'
-    )
-
-
-def test_int(user_config):
-    """
-    Test we see int as ints and not floats
-    """
-    valid_entries = [10, '2', 1.0]
-    invalid_entries = ['tough', '1.5', '']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckInt,
-        item='num_users'
-    )
-
-
-def test_datetime(user_config):
-    """
-    Test we see datetime as datetime
-    """
-    valid_entries = ['2018-01-10 10:10', '10-10-2018', "October 10 2018"]
-    invalid_entries = ['Not-a-date', 'Wednesday 5th']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckDatetime,
-        item='start_date'
-    )
-
-
-def test_list(user_config):
-    """
-    Test our listing methods using lists of dates.
-    """
-    valid_entries = [
-        '10-10-2019', ['10-10-2019'], ['10-10-2019', '11-10-2019']
-    ]
-
-    run_a_checker(
-        user_config, valid_entries, [], checkers.CheckDatetime, item='epochs'
-    )
-
-
-def test_directory(user_config):
-    """
-    Tests the base class for path based checkers
-    """
-    valid_entries = ["./"]
-    invalid_entries = ['./somecrazy_location!/']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckDirectory,
-        item='tmp'
-    )
-
-    # ISSUE #44 check for default when string is empty
-    user_config.cfg.update({'basic': {'tmp': ''}})
-
-    value = checkers.CheckDirectory(
-        config=user_config, section='basic', item='tmp'
-    ).cast()
-
-    assert os.path.split(value)[-1] == 'temp'
-
-
-def test_filename(user_config):
-    """
-    Tests the base class for path based checkers
-    """
-    # Remember paths are relative to the config
-    valid_entries = ["../test_checkers.py"]
-    invalid_entries = ['dumbfilename']
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckFilename,
-        item='log'
-    )
-
-
-def test_filename_empty_string(user_config):
-    """
-    ISSUE #44 check for default when string is empty
-    """
-    user_config.cfg.update({'basic': {'log': ''}})
-    user_config.mcfg.cfg['basic']['log'].default = None
-
-    value = checkers.CheckFilename(
-        config=user_config, section='basic', item='log'
-    ).cast()
-
-    assert value is None
-
-
-def test_filename_empty_string_default_path(user_config):
-    """
-    ISSUE #44 check for default when string is empty but default is a path
-    """
-    user_config.cfg.update({'basic': {'log': ''}})
-    user_config.mcfg.cfg['basic']['log'].default = 'log.txt'
-
-    value = checkers.CheckFilename(
-        config=user_config, section='basic', item='log'
-    ).cast()
-
-    assert os.path.split(value)[-1] == 'log.txt'
-
-
-def test_url(user_config):
-    """
-    Test our url checking.
-    """
-    valid_entries = ["https://google.com"]
-    invalid_entries = ["https://micah_subnaught_is_awesome.com"]
-
-    run_a_checker(
-        user_config,
-        valid_entries,
-        invalid_entries,
-        checkers.CheckURL,
-        item='favorite_web_site'
-    )
-
-
-def test_datetime_ordered_pairs(user_config):
-    """
-    Tests the ordered datetime pair checker which looks for <keyword>_start
-    <keyword>_end pairs and confirms they occurs in the correct order.
-    """
-    # Test end dates com after start dates
-    starts = ["1-01-2019", "2019-10-01", "1998-01-14 15:00:00"]
-    ends = ["1-02-2019", "2019-10-02", "1998-01-14 19:00:00"]
-
-    invalid_entries_starts = [
-        "01-01-2020", "2020-06-01", "1998-01-14 20:00:00"
-    ]
-    invalid_entries_ends = [
-        "01-01-2018", "2018-10-01", "1998-01-14 10:00:00"
-    ]
-
-    # Check for starts being before the end date
-    for start, end, error_start, error_end in zip(starts, ends,
-                                                  invalid_entries_starts,
-                                                  invalid_entries_ends):
-        # Check start values are before end values
-        acfg = {'basic': {'end_date': end}}
-
-        run_a_checker(
-            user_config,
-            [start],
-            [error_start],
-            checkers.CheckDatetimeOrderedPair,
-            item="start_date",
-            extra_config=acfg
+class TestCheckers:
+    @pytest.fixture(scope='class')
+    def master_config(self, test_config_dir):
+        """
+        Master config from test data
+        """
+        return MasterConfig(
+            path=test_config_dir.joinpath('master.ini').as_posix()
         )
 
-        # Check start values are before end values
-        acfg = {'basic': {'start_date': start}}
+    @pytest.fixture
+    def user_config(self, test_config_dir, master_config):
+        return UserConfig(
+            test_config_dir.joinpath('base_cfg.ini').as_posix(),
+            mcfg=master_config
+        )
+
+    def test_string(self, user_config):
+        """
+        Test we see strings as strings
+        """
+        valid_entries = ['test']
 
         run_a_checker(
             user_config,
-            [end],
-            [error_end],
+            valid_entries,
+            [],
+            checkers.CheckString,
+            item='username'
+        )
+
+        # Confirm that casting a string with uppers will auto produce lowers
+        user_config.cfg['basic']['username'] = 'Test'
+
+        b = checkers.CheckString(
+            config=user_config, section='basic', item='username'
+        )
+        result = b.cast()
+        assert result == 'test'
+
+        # Check we can capture a single item list for strings
+        b.is_list = True
+        result = b.cast()
+        assert result == ['test']
+
+        # Check we capture the when a list is passed and were not expecting one
+        b.is_list = False
+        result = b.cast()
+        assert not isinstance(result, list)
+
+    def test_bool(self, user_config):
+        """
+        Test we see booleans as booleans
+        """
+        valid_entries = [True, False, 'true', 'FALSE', 'yes', 'y', 'no', 'n']
+        invalid_entries = ['Fasle', 'treu']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckBool,
+            item='debug'
+        )
+
+    def test_float(self, user_config):
+        """
+        Test we see floats as floats
+        """
+        valid_entries = [-1.5, '2.5']
+        invalid_entries = ['tough']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckFloat,
+            item='time_out'
+        )
+
+    def test_int(self, user_config):
+        """
+        Test we see int as ints and not floats
+        """
+        valid_entries = [10, '2', 1.0]
+        invalid_entries = ['tough', '1.5', '']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckInt,
+            item='num_users'
+        )
+
+    def test_datetime(self, user_config):
+        """
+        Test we see datetime as datetime
+        """
+        valid_entries = ['2018-01-10 10:10', '10-10-2018', "October 10 2018"]
+        invalid_entries = ['Not-a-date', 'Wednesday 5th']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckDatetime,
+            item='start_date'
+        )
+
+    def test_list(self, user_config):
+        """
+        Test our listing methods using lists of dates.
+        """
+        valid_entries = [
+            '10-10-2019', ['10-10-2019'], ['10-10-2019', '11-10-2019']
+        ]
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            [],
+            checkers.CheckDatetime,
+            item='epochs'
+        )
+
+    def test_directory(self, user_config):
+        """
+        Tests the base class for path based checkers
+        """
+        valid_entries = ["./"]
+        invalid_entries = ['./somecrazy_location!/']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckDirectory,
+            item='tmp'
+        )
+
+        # ISSUE #44 check for default when string is empty
+        user_config.cfg.update({'basic': {'tmp': ''}})
+
+        value = checkers.CheckDirectory(
+            config=user_config, section='basic', item='tmp'
+        ).cast()
+
+        assert os.path.split(value)[-1] == 'temp'
+
+    def test_filename(self, user_config):
+        """
+        Tests the base class for path based checkers
+        """
+        # Remember paths are relative to the config
+        valid_entries = ["../test_checkers.py"]
+        invalid_entries = ['dumbfilename']
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckFilename,
+            item='log'
+        )
+
+    def test_filename_empty_string(self, user_config):
+        """
+        ISSUE #44 check for default when string is empty
+        """
+        user_config.cfg.update({'basic': {'log': ''}})
+        user_config.mcfg.cfg['basic']['log'].default = None
+
+        value = checkers.CheckFilename(
+            config=user_config, section='basic', item='log'
+        ).cast()
+
+        assert value is None
+
+    def test_filename_empty_string_default_path(self, user_config):
+        """
+        ISSUE #44 check for default when string is empty but default is a path
+        """
+        user_config.cfg.update({'basic': {'log': ''}})
+        user_config.mcfg.cfg['basic']['log'].default = 'log.txt'
+
+        value = checkers.CheckFilename(
+            config=user_config, section='basic', item='log'
+        ).cast()
+
+        assert os.path.split(value)[-1] == 'log.txt'
+
+    def test_url(self, user_config):
+        """
+        Test our url checking.
+        """
+        valid_entries = ["https://google.com"]
+        invalid_entries = ["https://micah_subnaught_is_awesome.com"]
+
+        run_a_checker(
+            user_config,
+            valid_entries,
+            invalid_entries,
+            checkers.CheckURL,
+            item='favorite_web_site'
+        )
+
+    def test_datetime_ordered_pairs(self, user_config):
+        """
+        Tests the ordered datetime pair checker which looks for <keyword>_start
+        <keyword>_end pairs and confirms they occurs in the correct order.
+        """
+        # Test end dates com after start dates
+        starts = ["1-01-2019", "2019-10-01", "1998-01-14 15:00:00"]
+        ends = ["1-02-2019", "2019-10-02", "1998-01-14 19:00:00"]
+
+        invalid_entries_starts = [
+            "01-01-2020", "2020-06-01", "1998-01-14 20:00:00"
+        ]
+        invalid_entries_ends = [
+            "01-01-2018", "2018-10-01", "1998-01-14 10:00:00"
+        ]
+
+        # Check for starts being before the end date
+        for start, end, error_start, error_end in zip(starts, ends,
+                                                      invalid_entries_starts,
+                                                      invalid_entries_ends):
+            # Check start values are before end values
+            acfg = {'basic': {'end_date': end}}
+
+            run_a_checker(
+                user_config,
+                [start],
+                [error_start],
+                checkers.CheckDatetimeOrderedPair,
+                item="start_date",
+                extra_config=acfg
+            )
+
+            # Check start values are before end values
+            acfg = {'basic': {'start_date': start}}
+
+            run_a_checker(
+                user_config,
+                [end],
+                [error_end],
+                checkers.CheckDatetimeOrderedPair,
+                item="end_date",
+                extra_config=acfg
+            )
+
+        # Check start end values are equal error
+        acfg = {'basic': {'start_date': '2020-10-01'}}
+
+        run_a_checker(
+            user_config,
+            ["2020-10-02"],
+            ["2020-10-01"],
             checkers.CheckDatetimeOrderedPair,
             item="end_date",
             extra_config=acfg
         )
 
-    # Check start end values are equal error
-    acfg = {'basic': {'start_date': '2020-10-01'}}
-
-    run_a_checker(
-        user_config,
-        ["2020-10-02"],
-        ["2020-10-01"],
-        checkers.CheckDatetimeOrderedPair,
-        item="end_date",
-        extra_config=acfg
-    )
-
-
-def test_bounds(user_config):
-    """
-    MasterConfig options now have max and min values to constrain continuous
-    types. This tests whether that works
-    """
-    run_a_checker(
-        user_config,
-        [1.0, 0.0, '0.5'],
-        [1.1, -1.0, '10'],
-        checkers.CheckFloat,
-        item='fraction'
-    )
+    def test_bounds(self, user_config):
+        """
+        MasterConfig options now have max and min values to constrain
+        continuous types. This tests whether that works
+        """
+        run_a_checker(
+            user_config,
+            [1.0, 0.0, '0.5'],
+            [1.1, -1.0, '10'],
+            checkers.CheckFloat,
+            item='fraction'
+        )
