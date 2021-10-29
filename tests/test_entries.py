a8727975fa31387eeb89f84ef2c9358d5259a66c
micah johnson <micah.johnson150@gmail.com>
2021-10-29 06:23:08 -0600

    Parametrized entries.py

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -7,66 +7,40 @@ test_recipe
 Tests for `inicheck.entries` module.
 """
 
-from collections import OrderedDict
-
 from inicheck.entries import ConfigEntry, RecipeSection, TriggerEntry
-
+import pytest
 
 class TestEntries():
-    def test_trigger_entry(self):
+    @pytest.mark.parametrize('trigger_str_list, expected_conditions_list', [
+        (['has_section = test'],  [['test', 'any', 'any']]),
+        (['has_item = test'],  [['any', 'test', 'any']]),
+        (['has_value = [test test test]'], [['test', 'test', 'test']]),
+        (['has_section = test', 'has_item = test2'], [['test', 'any', 'any'], ['any', 'test2', 'any']]),
+        (['has_value = [topo type ipw]'], [['topo', 'type', 'ipw']])
+    ])
+    def test_trigger_entry(self, trigger_str_list, expected_conditions_list):
         """
-        Tests to see if we correctly gather a trigger for a recipe
+        Triggers define the condition sets for which a recipe is applied. This tests that those are interepretted
+        correctly. A trigger can be defined by multiple triggers.
         """
-        entries = ['has_section = test',
-                   'has_item = test',
-                   'has_value = [test test test]',
-                   ['has_section = test',
-                    'has_item = test2']]
 
         # section trigger
-        t = TriggerEntry(entries[0])
-        assert(t.conditions[0] == ['test', 'any', 'any'])
+        t = TriggerEntry(trigger_str_list)
+        assert(t.conditions == expected_conditions_list)
 
-        # section item
-        t = TriggerEntry(entries[1])
-        assert(t.conditions[0] == ['any', 'test', 'any'])
-
-        t = TriggerEntry(entries[2])
-        assert(t.conditions[0] == ['test', 'test', 'test'])
-
-        # Confirm we can handle multiple conditions
-        t = TriggerEntry(entries[3])
-        assert(t.conditions[0] == ['test', 'any', 'any'])
-        assert(t.conditions[1] == ['any', 'test2', 'any'])
-
-    def test_config_tigger_entry(self):
+    @pytest.mark.parametrize("entry_str_list, expected_attribute, expected_value", [
+        (["default= [swe_z]", "type= string list"], 'default', ['swe_z']),
+        (["default= 10.5", "type= float"], 'default', '10.5'),
+        (["min = 2"], 'min', '2'),
+        # (["allow_none = true"], 'allow_none', 'true'),
+        # (["allow_none = false"], 'allow_none', False),
+        (["options = [auth guest]"], 'options', ['auth', 'guest']),
+        (["description = test"], 'description', 'test'),
+    ])
+    def test_config_entry(self, entry_str_list, expected_attribute, expected_value):
         """
-        Tests to see if we correctly gather a trigger for a recipe
+        This tests defining a master config entry from a string.
         """
-        entries = [['has_value = [topo type ipw]']]
+        e = ConfigEntry(name=None, parseable_line=entry_str_list)
+        assert getattr(e, expected_attribute) == expected_value
 
-        t = TriggerEntry(entries[0])
-        assert(t.conditions[0] == ['topo', 'type', 'ipw'])
-
-    def test_config(self):
-        """
-        User should be able to make a single item a list by requesting it by
-        with a master config entry like the following
-        ```
-            points_values_property:
-             default= [swe_z],
-             type= string list,
-             description = stuff
-         ```
-        Issue #20
-        """
-        s = ["default= [swe_z]",
-             "type= string list",
-             "description = stuff"]
-
-        e = ConfigEntry(name="points_values_property", parseable_line=s)
-
-        # Assert the defaults a signle item list
-        assert e.default == ['swe_z']
-        assert e.type == 'string'
-        assert e.listed == True
