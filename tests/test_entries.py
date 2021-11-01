b90423700ead8836bce9206ac60b3246eb121ea0
micah johnson <micah.johnson150@gmail.com>
2021-11-01 10:00:59 -0600

    Added in a test for the recipesection since it didnt have a test

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -10,10 +10,11 @@ Tests for `inicheck.entries` module.
 from inicheck.entries import ConfigEntry, RecipeSection, TriggerEntry
 import pytest
 
+
 class TestEntries():
     @pytest.mark.parametrize('trigger_str_list, expected_conditions_list', [
-        (['has_section = test'],  [['test', 'any', 'any']]),
-        (['has_item = test'],  [['any', 'test', 'any']]),
+        (['has_section = test'], [['test', 'any', 'any']]),
+        (['has_item = test'], [['any', 'test', 'any']]),
         (['has_value = [test test test]'], [['test', 'test', 'test']]),
         (['has_section = test', 'has_item = test2'], [['test', 'any', 'any'], ['any', 'test2', 'any']]),
         (['has_value = [topo type ipw]'], [['topo', 'type', 'ipw']])
@@ -26,11 +27,11 @@ class TestEntries():
 
         # section trigger
         t = TriggerEntry(trigger_str_list)
-        assert(t.conditions == expected_conditions_list)
+        assert (t.conditions == expected_conditions_list)
 
     @pytest.mark.parametrize("entry_str_list, expected_attribute, expected_value", [
         (["default= [swe_z]", "type= string list"], 'default', ['swe_z']),  # Test the list keyword in the type
-        (["default= [swe_z]", "type= string listed"], 'default', ['swe_z']), # Test the other list keyword in type
+        (["default= [swe_z]", "type= string listed"], 'default', ['swe_z']),  # Test the other list keyword in type
         (["default= 10.5", "type= float"], 'default', '10.5'),
         (["min = 2"], 'min', '2'),
         (["allow_none = true"], 'allow_none', True),
@@ -51,3 +52,36 @@ class TestEntries():
         """
         with pytest.raises(ValueError):
             e = ConfigEntry(name=None, parseable_line=['allow_none = ture'])
+
+class TestRecipeSection():
+
+    @pytest.fixture(scope='function')
+    def recipe(self, recipe_dict):
+        return RecipeSection(recipe_section_dict=recipe_dict)
+
+    @pytest.mark.parametrize("recipe_dict, trigger_names", [
+        # Test Simple trigger assignment
+        ({'test_trigger': 'has_item=username'}, ['test_trigger']),
+        # Test on config entry only that has no trigger
+        ({'section_adj': 'user_email=user1@internet.org'}, []),
+        # Test multiple triggers
+        ({'trigger1': 'has_section=test1', 'trigger2': 'has_item=test2'}, ['trigger1', 'trigger2']),
+    ])
+    def test_recipe_trigger_parse(self, recipe, recipe_dict, trigger_names):
+        """
+        Test the parsing and assignment of triggers
+        """
+        assert sorted(recipe.triggers.keys()) == sorted(trigger_names)
+
+    @pytest.mark.parametrize("recipe_dict, expected_adj_config", [
+        # Test Simple adjustment config assignment
+        ({'trigger': 'has_item=user', 'info': 'age=10'}, {'info': {'age': '10'}}),
+        # Test no adjustment config provided
+        ({'trigger': 'has_item=user'}, {}),
+
+    ])
+    def test_adj_config_parse(self, recipe, recipe_dict, expected_adj_config):
+        """
+        Test the parsing and assignment of adjustments if the conditions are met
+        """
+        assert recipe.adj_config == expected_adj_config
